b01c79a5eb2ec946f397c0e51b734711b22bcc33
rvthless <40124487+rvthless@users.noreply.github.com>
2020-06-15 15:14:56 +0200

    Update funkcjajakasmoze.py

--- funkcjajakasmoze.py
+++ funkcjajakasmoze.py
@@ -3,35 +3,38 @@ def transform(formula):
     if(formula[1] == "-" and formula[2] == ">"):
         print("NOT(", formula[0], ") v ", formula[3])
     
-    if(formula[1] == "<" and formula[2] == "-" and formula[3] == ">"):
+    elif(formula[1] == "<" and formula[2] == "-" and formula[3] == ">"):
         print("( NOT(", formula[0], ") v ", formula[4], ") AND ( NOT(", formula[4], ") v ", formula[0], " )")
     
-    if(formula[0:3] == "NOT" and formula[5:8] == "AND"):
+    elif(formula[0:3] == "NOT" and formula[5:8] == "AND"):
         print("NOT(", formula[4], ") v NOT(", formula[8], ")")
     
-    if(formula[0:3] == "NOT" and formula[5] == "v"):
+    elif(formula[0:3] == "NOT" and formula[5] == "v"):
         print("NOT(", formula[4], ") AND NOT(", formula[6], ")")
     
-    if(formula[0:6] == "NOTNOT"):
+    elif(formula[0:6] == "NOTNOT"):
         print(formula[6])
     
-    if(formula[2:5] == "AND" and formula[7] == "v"):
+    elif(formula[2:5] == "AND" and formula[7] == "v"):
         print("( ", formula[1], " v ", formula[8], " ) AND ( ", formula[5], " v ", formula[8], " )")
     
-    if(formula[2] == "v" and formula[5:8] == "AND"):
+    elif(formula[2] == "v" and formula[5:8] == "AND"):
         print("( ", formula[1], " AND ", formula[8], " ) v ( ", formula[3], " AND ", formula[8], " )")
     
-    if(formula[1] == "v"):
+    elif(formula[1] == "v"):
         print(formula[0])
     
-    if(formula[1:4] == "AND"):
+    elif(formula[1:4] == "AND"):
         print(formula[0])
     
-    if(formula[2:6] == "vNOT" and formula[8:11] == "AND"):
+    elif(formula[2:6] == "vNOT" and formula[8:11] == "AND"):
         print(formula[11])
     
-    if(formula[2:8] == "ANDNOT" and formula[10] == "v"):
+    elif(formula[2:8] == "ANDNOT" and formula[10] == "v"):
         print(formula[11])
+
+    else: 
+        print(formula)
 
 print("Hello, give the A part of the formula: ")
 
@@ -47,4 +50,8 @@ B = input()
 
 if(len(A) == 1):
     print(A, operator, end=" ")
+    transform(B)
+else:
+    transform(A)
+    print(operator, end=" ")
     transform(B)
