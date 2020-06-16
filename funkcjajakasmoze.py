ef3f1ad5929c3afe7f5ec7583d1d730637fbc650
rvthless <40124487+rvthless@users.noreply.github.com>
2020-06-16 21:38:07 +0200

    Update funkcjajakasmoze.py

--- funkcjajakasmoze.py
+++ funkcjajakasmoze.py
@@ -1,37 +1,37 @@
 def transform(formula):
 
-    if(formula[1] == "-" and formula[2] == ">"):
-        print("~(", formula[0], ") v ", formula[3])
+    if "->" in formula:
+        print("~( ", formula[0], " ) v", formula[3])
     
     elif "<->" in formula:
-        print("( ~(", formula[0], ") v ", formula[-1], ") & ( ~(", formula[0], ") v ", formula[-1], " )")
+        print("( ~( ", formula[0], " ) v ", formula[-1], " )& ( ~( ", formula[-1], ") v", formula[0], " )")
     
-    elif(formula[0:3] == "~" and formula[5:8] == "&"):
-        print("~(", formula[4], ") v ~(", formula[8], ")")
+    elif "~" in formula and "&" in formula:
+        print("~( ", formula[0], " ) v ~( ", formula[-1], " )")
     
-    elif(formula[0] == "~" and formula[5] == "v"):
-        print("~(", formula[4], ") & ~(", formula[6], ")")
+    elif "~" in formula and "v" in formula:
+        print("~( ", formula[2], " ) & ~( ", formula[-2], " )")
     
-    elif(formula[0:2] == "~~"):
-        print(formula[2])
+    elif "~~" in formula:
+        print(formula.replace('~~',''))
     
-    elif(formula[2:5] == "&" and formula[7] == "v"):
-        print("( ", formula[1], " v ", formula[8], " ) & ( ", formula[5], " v ", formula[8], " )")
+    elif formula.find('*\&*v') is not -1:
+        print("( ", formula[1], " v ", formula[6], " ) & ( ", formula[3], " v ", formula[6], " )")
     
-    elif(formula[2] == "v" and formula[5:8] == "&"):
-        print("( ", formula[1], " & ", formula[8], " ) v ( ", formula[3], " & ", formula[8], " )")
+    elif formula.find('*v*\&') is not -1:
+        print("( ", formula[1], " & ", formula[6], " ) v ( ", formula[3], " & ", formula[6], " )")
     
-    elif(formula[1] == "v" and formula[0] == formula[2]):
+    elif "v" in formula and formula[0] == formula[-1]:
         print(formula[0])
     
-    elif(formula[1] == "&" and formula[0] == formula[2]):
+    elif "&" in formula and formula[0] == formula[-1]:
         print(formula[0])
     
-    elif(formula[2:6] == "v~" and formula[8:11] == "&"):
-        print(formula[11])
+    elif ("v~" and "&") in formula and formula[1] == formula[4]:
+        print(formula[7])
     
-    elif(formula[2:8] == "&~" and formula[10] == "v"):
-        print(formula[11])
+    elif("&~" and "v") in formula and formula[1] == formula[4]:
+        print(formula[7])
 
     else: 
         print(formula)
@@ -48,10 +48,7 @@ print("Now give B part of the formula: ")
 
 B = input()
 
-if(len(A) < 3 and len(B) < 3):
-    full = A + ' ' + operator + ' ' + B
-    transform(full)
-elif(len(A) < 3):
+if(len(A) == 1):
     print(A, operator, end=" ")
     transform(B)
 else:
