a831169a8c634f636a67b519c1b47c2776a68c73
rvthless <40124487+rvthless@users.noreply.github.com>
2020-06-16 23:22:33 +0200

    Update funkcjajakasmoze.py

--- funkcjajakasmoze.py
+++ funkcjajakasmoze.py
@@ -3,7 +3,7 @@ import re
 def transform(formula):
     a = ''
     if "->" in formula:
-        a =  "~( "+formula[0]+" ) v "+formula[3]
+        a =  "~( "+formula[0]+" ) v "+formula[-1]
     
     elif "<->" in formula:
         a = "( ~( "+formula[0], " ) v "+formula[-1]+" ) & ( ~( "+formula[-1]+") v"+formula[0]+" )"
@@ -38,6 +38,36 @@ def transform(formula):
     else: 
         a = formula
     return a
+    
+def transform2(formula):
+    a = ''
+    if "->" in formula:
+        a =  "~( "+formula[0]+" ) v "+formula[-1]
+    
+    elif "<->" in formula:
+        a = "( ~( "+formula[0], " ) v "+formula[-1]+" ) & ( ~( "+formula[-1]+") v"+formula[0]+" )"
+    
+    elif "~" in formula and "&" in formula:
+        a = "~( "+ formula[0]+ " ) v ~( "+ formula[-1]+ " )"
+    
+    elif "~" in formula and "v" in formula:
+        a = "~( "+ formula[2]+ " ) & ~( "+ formula[-2]+ " )"
+    
+    elif "v" in formula and formula[0] == formula[-1]:
+        a = formula[0]
+    
+    elif "&" in formula and formula[0] == formula[-1]:
+        a = formula[0]
+    
+    elif "v~" in formula and "&" in formula and formula[1] == formula[4]:
+        a = formula[7]
+    
+    elif "&~" in formula and "v" in formula and formula[1] == formula[4]:
+        a = formula[7]
+
+    else: 
+        a = formula
+    return a
 
 print("Hello, give the A part of the formula: ")
 
@@ -54,7 +84,13 @@ B = input()
 if(len(A) < 3):
     b = transform(B)
     print(A, operator, b)
+    table = [A, operator, b]
+    ans = transform2(table)
+    print(ans)
 else:
     a = transform(A)
     b = transform(B)
     print( a , operator, b )
+    table = [a, operator, b]
+    ans = transform2(table)
+    print(ans)
